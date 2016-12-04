f58dbc8013954bdce0bcfba2782759434ae96ae0
ilex0208 <tiangonglei@126.com>
2016-12-04 09:27:23 +0800

    *)修改webpack配置

--- amos-overtDemo/webpack.config.js
+++ amos-overtDemo/webpack.config.js
@@ -1,26 +1,91 @@
-module.exports = {
-  entry: {
-    index: './index.js',
-    pstn: './pstn.js',
-    container: './container.js',
-    performance: './performance.js',
-    statictis: './statictis.js',
-    mix: './mix.js',
-    animate: './animate.js',
-    layout: './layout.js'
-  },
-  output: {
-    path: './dist', // 编译到当前目录
-    publicPath: '/dist/', // 服务器根路径
-    filename: '[name].js' // 编译后的文件名字
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-      { test: /\.css$/, loader: 'style-loader!css-loader?sourceMap' }
-    ]
-  },
+/**
+ * webpack 打包配置文件
+ * @author ilex
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+
+const __path = 'dist';
+// ------------------------------------
+// Environment
+// ------------------------------------
+const __DEV__ = 'development';
+
+// 发布模式
+// process.env.NODE_ENV = 'production';
+
+console.log('>创建webpack配置文件.');
+
+const webpackConfig = {
+  name: 'amos',
+  target: 'web',
   resolve: {
-    extensions: ['', '.js', '.jsx']
-  }
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  module: {},
+  plugins:[]
 };
+
+// ------------------------------------
+// 入口点
+// ------------------------------------
+webpackConfig.entry = {
+  index: './index.js',
+  pstn: './pstn.js',
+  container: './container.js',
+  performance: './performance.js',
+  statictis: './statictis.js',
+  mix: './mix.js',
+  animate: './animate.js',
+  layout: './layout.js'
+};
+
+// ------------------------------------
+// Bundle Output
+// ------------------------------------
+webpackConfig.output = {
+  path: path.join(__dirname, __path), // 编译到当前目录
+  publicPath: '/dist/', // 服务器根路径
+  filename: '[name].js' // 编译后的文件名字
+};
+
+// ------------------------------------
+// Loaders
+// ------------------------------------
+webpackConfig.module.loaders = [
+  { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+  { test: /\.css$/, loader: 'style-loader!css-loader?sourceMap' }
+];
+
+// 启动发布模式
+if (process.env.NODE_ENV === 'production') {
+  console.log('>启动发布模式(production).');
+  webpackConfig.plugins.push(
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  );
+  module.exports.devtool = '#source-map';
+}
+
+if(__DEV__){
+  console.log('>启动实时预览插件(development) (HMR, NoErrors).');
+  webpackConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  );
+}
+
+module.exports = webpackConfig;
